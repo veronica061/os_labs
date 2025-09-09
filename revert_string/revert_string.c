a638bc7c8f11fff874062b60da7e0774b85b3c0d
veronica061 <98640736+veronica061@users.noreply.github.com>
2025-09-09 14:26:17 +0000

    Добавлены папки второй ЛР

--- revert_string/revert_string.c
+++ revert_string/revert_string.c
@@ -1,7 +1,18 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    if (str == NULL) return;
+    
+    int length = strlen(str);
+    int i = 0;
+    int j = length - 1;
+    while (i < j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
 }
-
--- swap/swap.c
+++ swap/swap.c
@@ -2,5 +2,8 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+
+    char temp = *left;
+    *left = *right;
+    *right = temp;
 }
